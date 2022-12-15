23693d4bec2641557259fd12bbf06e56f78fdc1d
pavel <dongod@bk.ru>
2022-12-15 19:03:47 +0400

    1212

---
+++ task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int FillArray()
+{
+    Console.WriteLine("Введите числа, для запуска программы ведите 0");
+    bool loop = true;
+    int count = 0;
+    while (loop)
+    {
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num > 0)
+        {
+            count++;
+        }
+        else if (num == 0)
+        {
+            loop = false;
+        }
+    }
+    return count;
+}
+
+void PrintArray(int array)
+{
+    Console.WriteLine($"Вы ввели {(array)} положительных чисел");
+}
+
+int array = FillArray();
+PrintArray(array);
+
+
+
